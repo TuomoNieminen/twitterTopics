f02cf0f2ae0c92063cee24ec1b37d8671eff22e5
TuomoNieminen <tuomo.a.nieminen@helsinki.fi>
2016-04-01 12:53:31 +0300

    added smappR libraries

--- twitter_functions.R
+++ twitter_functions.R
@@ -1,6 +1,23 @@
-# functions to search and clean twitter data
-# 26.3.2016
-# tuomo.a.nieminen@gmail.com
+
+# author: tuomo.a.nieminen@gmail.com
+
+if(!require(RCurl)) {install.packages("RCurl"); library(RCurl)}
+if(!require(httr)) {install.packages("httr"); library(httr)}
+set_config( config( ssl_verifypeer = 0L ) )
+
+if(!require(devtools)) {install.packages("devtools"); library(devtools)}
+
+install <- FALSE # set this to true if you need to install the packages below
+
+doInstall <- c("ROAuth", "igraph", "ggplot2", "wordcloud", "devtools", "tm",
+               "R2WinBUGS", "rmongodb", "scales")
+if(install) {
+  install.packages(toInstall, repos = "http://cran.r-project.org")
+  install_github("streamR", "pablobarbera", subdir="streamR")
+  install_github("smappR", "SMAPPNYU")
+}
+library(streamR)
+library(smappR)
 
 if(!require(twitteR)){ install.packages("twitteR"); library(twitteR)}
 if(!require(slam)) { install.packages("slam"); library("slam")}
@@ -15,14 +32,13 @@ if(!require(ggplot2)) {install.packages("ggplot2"); library(ggplot2)}
 if(!require(plyr)) {install.packages("plyr"); library(plyr)}
 library(grid)
 
-# Authentication to twitter. 
-# You need to register an 'App' to your twitter account and get the needed info.
-twitter_oauth <- function(ck,cs,at,as) {
+#Authentication to twitter
+twitter_oauth <- function() {
   
-  setup_twitter_oauth(consumer_key = ck,
-                      consumer_secret = cs,
-                      access_token = at,
-                      access_secret = as)
+  setup_twitter_oauth(consumer_key = "HcRvVGPXbvk1DUjXqPlgQOmpu",
+                      consumer_secret = "0ZFUgXpBDA9P4v0EHfEawTKhdVIaatwHFHNVQbfWCcgnixoWyo",
+                      access_token = "468183756-pHBBfre2TL6VKXJ2QqaAAO73mVt7D2jZrvbJQ3Jj",
+                      access_secret = "cOT1M9mz2jWpy47BmKkk9WC3wlwwNeQfUihBLxFynAUhP")
 }
 
 
@@ -57,7 +73,7 @@ super_search <- function(search = "userTimeline",
   return(tweets)
 }
 
-# subset tweetdata by date
+# get tweetdata by date
 
 get_datedata <- function(tweet_df, date="2015-12-16") {
   format <- ifelse(nchar(date)>8,"%Y-%m-%d","%Y-%m")
@@ -73,7 +89,8 @@ get_languages<-function(text_v, init_langs) {
   textcat(text_v, p = profile)
 }
 
-# get a document term matrix from a vector of texts (tweets)
+# returns a term document matrix from a vector of texts (tweets)
+# used by twitter_wordfreq()
 get_documentTermMatrix <- function(search_text, searchwords=NULL,language="fi") {
   
   cleantext <- clean_text(search_text)
@@ -103,7 +120,7 @@ get_documentTermMatrix <- function(search_text, searchwords=NULL,language="fi") 
 }
 
 
-# get a sorted data frame with word frequencies
+# returns a sorted data frame with word frequencies
 # example:
 # tweets <- searchTwitter("talous", since="2016-01-01")
 # tw_df <- twListToDF(tweets)
@@ -125,7 +142,9 @@ twitter_wordfreqs <- function(documentTermMatrix) {
   return(dm) 
 }
 
-# take a vector of tweets (text) and get the frequency of hastags
+# a function that takes a vector of tweets (text) and 
+# returns the frequency of hastags
+
 # example: 
 # tw = userTimeline("BarackObama", cainfo = x1, n = 3200)
 # tw = twListToDF(tw)
@@ -146,7 +165,7 @@ extract_hashes = function(tweet_text){
   return(df)
 }
 
-# remove unwanted characters from text
+# removes unwanted characters from text
 clean_text <- function(some_txt)
 {
   some_txt = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", some_txt)
@@ -176,9 +195,8 @@ clean_text <- function(some_txt)
 }
 
 
-# below are some helper functions for anaysis and plotting
+#popularity
 
-# popularity by date
 get_popularity_bydate <- function(data,period="monthly") {
   form <- switch(period,
                  "monthly" = "%Y %m",
@@ -201,28 +219,184 @@ get_popularity_bydate <- function(data,period="monthly") {
   
 }
 
-# ... data for a plot
 get_summary <- function(data,by="kuukausi") {
-if(by=="kuukausi") {
-  data$aika <- format(data$created,"%Y %m")
+  if(by=="kuukausi") {
+    data$aika <- format(data$created,"%Y %m")
+    
+    data$keski_suosio <- sapply(data$aika, function(m) {
+      sub <- data[data$aika==m,]
+      mean(sub$suosio)
+    })
+    
+  } else {
+    data$aika <- data$tunti
+    data$keski_suosio <- sapply(data$aika, function(h) {
+      sub <- data[data$aika==h,]
+      mean(sub$suosio)
+    })
+  }
   
-  data$keski_suosio <- sapply(data$aika, function(m) {
-    sub <- data[data$aika==m,]
-    mean(sub$suosio)
-  })
-  
-} else {
-  data$aika <- data$tunti
-data$keski_suosio <- sapply(data$aika, function(h) {
-  sub <- data[data$aika==h,]
-  mean(sub$suosio)
-})
+  ddply(
+    data,
+    .(aika, keski_suosio),
+    summarize,
+    tweets=length(aika)
+  )
 }
 
-ddply(
-  data,
-  .(aika, keski_suosio),
-  summarize,
-  tweets=length(aika)
-)
+# slightly modified from getTimeline in smappR package
+my_getTimeline <- function(filename, n=3200, oauth_folder="credentials", screen_name=NULL, 
+                        id=NULL, max_id = NULL, since_id=NULL, trim_user="true", sleep=.5, verbose=FALSE){
+  
+  ## create list of credentials
+  creds <- list.files(oauth_folder, full.names=T)
+  ## open a random credential
+  cr <- sample(creds, 1)
+  if (verbose) message(cr)
+  load(cr)
+  ## while rate limit is 0, open a new one
+  limit <- getLimitTimeline(my_oauth)
+  if (verbose) message(limit, " hits left")
+  while (limit==0){
+    cr <- sample(creds, 1)
+    if (verbose) message(cr)
+    load(cr)
+    Sys.sleep(sleep)
+    # sleep for 5 minutes if limit rate is less than 100
+    rate.limit <- getLimitRate(my_oauth)
+    if (rate.limit<100){
+      Sys.sleep(300)
+    }
+    limit <- getLimitTimeline(my_oauth)
+    if (verbose) message(limit, " hits left")
+  }
+  ## url to call
+  url <- "https://api.twitter.com/1.1/statuses/user_timeline.json"
+  
+  ## first API call
+  if (!is.null(screen_name)){
+    params <- list(screen_name = screen_name, count=200, trim_user=trim_user)
+  }
+  if (!is.null(id)){
+    params <- list(id=id, count=200, trim_user=trim_user)   
+  }
+  if (!is.null(since_id)){
+    params[["since_id"]] <- since_id
+  }
+  if(!is.null(max_id)) {
+    params[["max_id"]] <- max_id
+  }
+  query <- lapply(params, function(x) URLencode(as.character(x)))
+  
+  # preparing OAuth token for httr
+  options("httr_oauth_cache"=FALSE)
+  app <- httr::oauth_app("twitter", key = my_oauth$consumerKey, 
+                         secret = my_oauth$consumerSecret)
+  credentials <- list(oauth_token = my_oauth$oauthKey, oauth_token_secret = my_oauth$oauthSecret)
+  twitter_token <- httr::Token1.0$new(endpoint = NULL, params = list(as_header = TRUE), 
+                                      app = app, credentials = credentials)
+  
+  # first query
+  url.data <- httr::GET(url, query = query, httr::config(token = twitter_token))
+  Sys.sleep(sleep)
+  ## one API call less
+  limit <- limit - 1
+  ## changing oauth token if we hit the limit
+  if (verbose) message(limit, " hits left")
+  cr_old <- cr
+  while (limit==0){
+    cr <- sample(creds, 1)
+    if (verbose) message(cr)
+    load(cr)
+    Sys.sleep(sleep)
+    # sleep for 5 minutes if limit rate is less than 100
+    rate.limit <- getLimitRate(my_oauth)
+    if (rate.limit<100){
+      Sys.sleep(300)
+    }
+    limit <- getLimitTimeline(my_oauth)
+    if (verbose) message(limit, " hits left")
+  }
+  if (cr != cr_old) {
+    app <- httr::oauth_app("twitter", key = my_oauth$consumerKey, 
+                           secret = my_oauth$consumerSecret)
+    credentials <- list(oauth_token = my_oauth$oauthKey, oauth_token_secret = my_oauth$oauthSecret)
+    twitter_token <- httr::Token1.0$new(endpoint = NULL, params = list(as_header = TRUE), 
+                                        app = app, credentials = credentials)
+  }
+  ## trying to parse JSON data
+  ## json.data <- fromJSON(url.data, unexpected.escape = "skip")
+  json.data <- httr::content(url.data)
+  if (length(json.data$error)!=0){
+    message(url.data)
+    stop("error! Last cursor: ", cursor)
+  }
+  ## writing to disk
+  conn <- file(filename, "a")
+  ret <- lapply(json.data, function(x) writeLines(jsonlite::toJSON(x, null="null"), con=conn))
+  close(conn)
+  ## max_id
+  tweets <- length(json.data)
+  max_id <- json.data[[tweets]]$id_str
+  message(tweets, " tweets. Max id: ", max_id)
+  max_id_old <- "none"
+  if (is.null(since_id)) {since_id <- 1}
+  
+  while (tweets < n & max_id != max_id_old & 
+         as.numeric(max_id) > as.numeric(since_id)){
+    max_id_old <- max_id
+    if (!is.null(screen_name)){
+      params <- list(screen_name = screen_name, count=200, max_id=max_id,
+                     trim_user=trim_user)
+    }
+    if (!is.null(id)){
+      params <- list(id=id, count=200, max_id=max_id, trim_user=trim_user)
+    }
+    if (!is.null(since_id) && since_id != 1 ){
+      params[['since_id']] <- since_id
+    }
+    query <- lapply(params, function(x) URLencode(as.character(x)))
+    url.data <- httr::GET(url, query = query, httr::config(token = twitter_token))
+    Sys.sleep(sleep)
+    ## one API call less
+    limit <- limit - 1
+    ## changing oauth token if we hit the limit
+    message(limit, " hits left")
+    cr_old <- cr
+    while (limit==0){
+      cr <- sample(creds, 1)
+      message(cr)
+      load(cr)
+      Sys.sleep(sleep)
+      # sleep for 5 minutes if limit rate is less than 100
+      rate.limit <- getLimitRate(my_oauth)
+      if (rate.limit<100){
+        Sys.sleep(300)
+      }
+      limit <- getLimitTimeline(my_oauth)
+      message(limit, " hits left")
+    }
+    if (cr != cr_old) {
+      app <- httr::oauth_app("twitter", key = my_oauth$consumerKey, 
+                             secret = my_oauth$consumerSecret)
+      credentials <- list(oauth_token = my_oauth$oauthKey, oauth_token_secret = my_oauth$oauthSecret)
+      twitter_token <- httr::Token1.0$new(endpoint = NULL, params = list(as_header = TRUE), 
+                                          app = app, credentials = credentials)
+    }
+    ## trying to parse JSON data
+    ## json.data <- fromJSON(url.data, unexpected.escape = "skip")
+    json.data <- httr::content(url.data)
+    if (length(json.data$error)!=0){
+      message(url.data)
+      stop("error! Last cursor: ", cursor)
+    }
+    ## writing to disk
+    conn <- file(filename, "a")
+    ret <- lapply(json.data, function(x) writeLines(jsonlite::toJSON(x, null="null"), con=conn))
+    close(conn)
+    ## max_id
+    tweets <- tweets + length(json.data)
+    max_id <- json.data[[length(json.data)]]$id_str
+    message(tweets, " tweets. Max id: ", max_id)
+  }
 }
